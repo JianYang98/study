8e9fbaa8647445b3511a0b2ab13f7eb3614f0cfe
JianYang98 <121104065+JianYang98@users.noreply.github.com>
2024-04-19 22:41:21 +0900

    [Silver IV] Title: 수 찾기, Time: 220 ms, Memory: 51320 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -5,18 +5,14 @@ nList = sorted(list(map(int,sys.stdin.readline().split()))) # O(N log N)
 m = int(sys.stdin.readline().strip())
 mList = list(map(int,sys.stdin.readline().split()))
 
-def binary(element, min , max ) :
-    mid = (min+ max) //2 
-    if min > max :        
-        return 0 
-    if mid >=n:
-        return 0
-    if element == nList[mid]:
-        return 1
-    if element > nList[mid]:
-        return binary(element,mid+1, max )
-    else :
-        return  binary(element,min, mid-1 )
+dic = {}
+
+for x in nList:
+    if x not in dic:
+        dic[x] = 1
 
 for y in mList :
-    print(binary(y,0,n-1)  )
+    if y in dic :
+        print(dic[y] )
+    else:
+        print(0)
