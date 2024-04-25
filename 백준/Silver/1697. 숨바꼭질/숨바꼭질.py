bb5bbcd9ab9a3a6c6253a01fadda5d19d987bd99
JianYang98 <121104065+JianYang98@users.noreply.github.com>
2024-04-26 07:01:08 +0900

    [Silver I] Title: 숨바꼭질, Time: 104 ms, Memory: 35268 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,29 @@
+import sys
+from collections import deque
+sys.setrecursionlimit(10**6)
+
+n , k = map(int , sys.stdin.readline().split())
+filedList = [0]*100001
+
+
+
+def bfs(start) :
+    bqueue = deque([start]) # 노드 , 시간 
+    while bqueue :
+        v  = bqueue.popleft()        
+        if k == v : # 동생 V 랑 bfs랑 같으면 걍 멈춰
+            return filedList[v]
+        for next_v  in (v-1 , v+1 ,v*2 ) :
+            if 0<=next_v<=100000 and not filedList[next_v ] :
+                filedList[next_v ]= filedList[v] + 1
+                bqueue.append(next_v)
+
+        
+
+
+
+print(bfs(n))
+
+
+
+
