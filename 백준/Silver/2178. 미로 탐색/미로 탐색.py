f454fa9e5e0c68ddc9c13a0dbbc96d637ed6c50d
JianYang98 <121104065+JianYang98@users.noreply.github.com>
2024-04-27 23:45:00 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34068 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,35 +1,42 @@
 import sys
 from collections import deque
 
-n, m = map(int , sys.stdin.readline().split())
 
-miro= []
+def BFS():
+    bqueue = deque([(0, 0)])
+    dx = [0, 0, 1, -1]
+    dy = [1, -1, 0, 0]
 
-for _ in range(n) :
-    miro.append( list(map(int,sys.stdin.readline().strip())))
-
-n -=1
-m -=1
-
-def BFS ():
-    bqueue = deque([(0,0)])
-    dx = [0,0,1,-1]
-    dy = [1,-1,0,0]
-    
-    while bqueue :
-        by , bx = bqueue.popleft()
-        #print(by," ", bx)
-        if n == by and m == bx :
+    while bqueue:
+        by, bx = bqueue.popleft()
+        if n == by and m == bx:
             print(miro[by][bx])
             break
         for i in range(4):
             fy = by + dy[i]
             fx = bx + dx[i]
-            if 0<=fy<n+1 and 0<=fx< m+1 :
-                #print("fy: " , fy , " fx:", fx) 
-                if miro[fy][fx] == 1 :
-                    miro[fy][fx] = miro[by][bx] + 1 
-                    bqueue.append((fy,fx))
+            if 0 <= fy < n + 1 and 0 <= fx < m + 1:
+                if miro[fy][fx] == 1:
+                    miro[fy][fx] = miro[by][bx] + 1
+                    bqueue.append((fy, fx))
+
+if __name__ == "__main__" :
+
+    n, m = map(int , sys.stdin.readline().split())
     
-BFS()   
+    miro= []
+    
+    for _ in range(n) :
+        miro.append( list(map(int,sys.stdin.readline().strip())))
+    
+    n -=1
+    m -=1
+    
+    BFS()
+
+
+
+
+
+
 
