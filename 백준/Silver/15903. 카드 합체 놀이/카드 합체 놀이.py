b122000671c0d29b3fae3f263325ec77ca73e902
JianYang98 <121104065+JianYang98@users.noreply.github.com>
2024-06-28 19:57:35 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 56 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
@@ -1,15 +1,16 @@
 import sys
+import heapq
 
 n , m = map(int,sys.stdin.readline().split())
 
 tList = list(map(int,sys.stdin.readline().split()))
+heapq.heapify(tList)
 
 for _ in range (m) :
-    tList.sort()
-    a = tList[0]
-    b = tList[1]
-    tList[0] = a+b
-    tList[1] = a+b
+    x = heapq.heappop(tList)
+    y = heapq.heappop(tList)
+    heapq.heappush(tList,x+y)
+    heapq.heappush(tList,x+y)
 
 print(sum(tList))
     
