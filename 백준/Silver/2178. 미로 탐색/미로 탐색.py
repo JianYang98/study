3379c412ba6040887ee61481620daf808be70c76
JianYang98 <121104065+JianYang98@users.noreply.github.com>
2024-10-13 02:41:00 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34204 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,42 +1,41 @@
 import sys
 from collections import deque
+sys.setrecursionlimit(10**6)
+
+#sys.stdin = open("input.txt", "r")
 
 
-def BFS():
-    bqueue = deque([(0, 0)])
+def BFS ():
+    global n, m
+    bqueue  = deque([(0,0)])
     dx = [0, 0, 1, -1]
     dy = [1, -1, 0, 0]
 
-    while bqueue:
-        by, bx = bqueue.popleft()
-        if n == by and m == bx:
-            print(miro[by][bx])
+    while bqueue :
+        by  , bx = bqueue.popleft()
+        #print(by, " ", bx)
+        #print(n, " n : m  ", m)
+        if n  ==by+1 and m == bx+1 :
+            print(nList[by][bx])
             break
+
+        # 큐에서 값 pop()
         for i in range(4):
             fy = by + dy[i]
             fx = bx + dx[i]
-            if 0 <= fy < n + 1 and 0 <= fx < m + 1:
-                if miro[fy][fx] == 1:
-                    miro[fy][fx] = miro[by][bx] + 1
-                    bqueue.append((fy, fx))
+            if 0<=fy<n and 0<=fx< m : #상하좌우 탐색 할 좌표 값 범위 체크
+                if nList[fy][fx] == 1 :
+                    nList[fy][fx] = nList[by][bx] + 1
+
+                    bqueue.append((fy,fx))
+
+
+
 
 if __name__ == "__main__" :
-
-    n, m = map(int , sys.stdin.readline().split())
-    
-    miro= []
-    
-    for _ in range(n) :
-        miro.append( list(map(int,sys.stdin.readline().strip())))
-    
-    n -=1
-    m -=1
-    
+    n , m = map(int, sys.stdin.readline().split())
+    nList = []
+    for __ in range(n):
+        nList.append(list(map(int, sys.stdin.readline().strip())))
     BFS()
-
-
-
-
-
-
 
