6622856b5ed8b20ffcc464e0d9890922acbb2f82
JianYang98 <121104065+JianYang98@users.noreply.github.com>
2024-10-16 22:07:11 +0900

    [Silver III] Title: 크면서 작은 수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.py
+++ 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.py
@@ -1,41 +1,32 @@
 import sys
-   
-#입력된 숫자들을 자른다. 
-#숫자들의 조합을 DFS 로 만든다.
-# 만들어진 숫자가 제시된 숫자보다 클때 list에 넣는다.
-# 마지막 list의 min을 출력한다. 
 
 n = int(sys.stdin.readline().strip())
-
-nList =list(map(int,(str(n))))
-res =[False] * len(nList)
-tmp = []
+m = list(map(int,str(n)))
+tmpMin = float('inf')
+resultTmp=[]
 result = []
+visited = [False] * len(m)
+def DFS(N):
+    global tmpMin 
+    before = float('inf')
+    
+    if len(result) == len(m) :
+        currentValue =  int(''.join(map(str,result))) 
+        if n < currentValue :
+            resultTmp.append(currentValue)
+            return ;
+    for x in range (len(m)):
+        if visited[x] == False and m[x]!=before :
+            result.append(m[x])
+            visited[x] = True
+            DFS(N+1)
+            before = result.pop()
+            visited[x] = False
 
-def DFS():
-    check = 0 #pop하기 전 값을 세팅하기 위한 값
+DFS(0)
 
-    if len(tmp) == len(nList):
-        # 숫자가 들어있는 리스트를 str로 변경해서 한줄로 만들고 int 
-        currentValue = int(''.join(map(str, tmp)))
-        if currentValue > n :
-            result.append(currentValue)
-
-            
-        return
-    else : 
-        for x in range(len(nList)):
-            if res[x] == False and check != nList[x] :
-                tmp.append(nList[x])
-                check = nList[x]
-                res[x] = True
-                DFS()
-                tmp.pop()
-                res[x] = False
-                
-
-DFS()
-if len(result) == 0 :
+if len(resultTmp) == 0 :
     print(0)
-else:
-    print(min(result))
+else :
+    print(min(resultTmp))
+
