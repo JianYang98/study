62f538533e9b3ae6eb8b0c2afa8147ad5fd1d86c
JianYang98 <121104065+JianYang98@users.noreply.github.com>
2024-10-17 22:27:26 +0900

    [Gold V] Title: 토마토, Time: 1584 ms, Memory: 155312 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,60 +1,57 @@
-from collections import deque
 import sys
-
-# m 가로 , n 세로 
-m , n = map(int,sys.stdin.readline().split())
-tList = []
-#vistied = [[ -1 for x in range (m) ] for _ in range(n)]
-tomato = []
-for x in range(n) :
-    tmpList = list( map(int,sys.stdin.readline().split()))
-    tList.append(tmpList)
+from collections import deque
+#sys.stdin = open("input.txt", "r")
 
 
-# 익은 토마토 위치 찾기
-q = deque()
-for i in range(n):
-    for j in range(m):
-        if tList[i][j] == 1:
-            q.append((i, j))
+def BFS (toCnt):
+    global  n , m
+    #print("toCnt ",toCnt) # 토마토 갯수
+    day = -1
+    tomatoCont = len(tomamoTmp)
+    tomamo= deque(tomamoTmp)
+    while tomamo :
+        x ,y ,nowday= tomamo.popleft()
+        #print(x,y)
+        day = max(day ,nowday )
+
+        visited[y][x] = True
+        dx = [-1,1,0,0]
+        dy = [0,0,-1,1]
+
+        for a in range(4):
+            tx = x +dx[a]
+            ty = y +dy[a]
+            if 0<=tx<m and 0<=ty<n : # 범위값안일때
+                #print("tx : ",tx ," ty :",ty)
+                if visited[ty][tx] == False and  tList[ty][tx] ==0:
+                    visited[ty][tx] = True
+                    tomatoCont += 1
+                    tomamo.append((tx,ty,nowday+1))
+    #print(tomatoCont)
+    if tomatoCont == toCnt :
+        print( day )
+    elif tomatoCont <toCnt :
+        print(-1)
 
 
+if __name__ == "__main__" :
+    m, n = map(int, sys.stdin.readline().split()) # m 은 x ,n는 y임
+    tomamoTmp = []
+    visited = [[False  for __ in range(m)] for __ in range(n)]
+    #print(visited)
+    tList = []
+    ne = 0 # -1 갯수
+    cnt = 0
+    toCnt = 0
+    for y in range(n):
+        t = list(map(int, sys.stdin.readline().split()))
+        # print(t)
+        tList.append(t)
+        for a in range(m):
+            if t[a] == 1:
+                tomamoTmp.append((a, y,0))
+            elif t[a] == -1:
+                ne+=1
+    toCnt = (n * m )- ne # 총 카운트 해야하는 토마토 갯수
 
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
-day = -1 
-while q :
-    day +=1
-    for _ in range(len(q)):
-        qx , qy = q.popleft()
-        #print("qx , qy", qx , qy)
-        for i in range(4):
-            tx = qx+dx[i]
-            ty = qy+dy[i]
-            #print("tx , ty", tx , ty)
-            #print ( n,m)
-            if 0<=tx< n and 0<=ty< m :
-                if tList[tx][ty] == 0  :
-                    #print(1)
-                    tList[tx][ty]  = tList[tx][ty] + 1
-                    q.append((tx,ty))
-
-verify = True
-for i in range(n):
-    if 0 in tList[i]:
-        verify = False
-        break
-
-
-if verify :
-    print(day)    
-else :
-    print(-1)
-
-
-
-
-
-
-    
-
+    BFS(toCnt)
