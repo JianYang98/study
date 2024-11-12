61574a1ad0cd32f36cd16dbc478f49d2ce74ce07
JianYang98 <121104065+JianYang98@users.noreply.github.com>
2024-11-12 22:51:52 +0900

    [Gold IV] Title: 타임머신, Time: 660 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.py
+++ 백준/Gold/11657. 타임머신/타임머신.py
@@ -1,39 +1,45 @@
 import sys
+#sys.stdin = open("input.txt" , "r")
+# 8:34
 
-n , m = map(int, sys.stdin.readline().split())
-distance = [(float('INF'))] * (n+1) 
-edges = []
+INF = float('inf')
 
-for _ in range(m):
-    a , b, c =  map(int, sys.stdin.readline().split()) 
-    edges.append((a,b,c))
-
-def df(start) :
+def bef(start):
     distance[start] = 0
-    # n-1 번 순회하쥬
-    # 정점만큼 순회 
-    for x in range(n) :
-        # 간선만큼 순회
-        for y in range(m) : 
-            now = edges[y][0]
-            next_node = edges[y][1]
-            cost = edges[y][2]
-            if now != (float('INF')) and distance[next_node] > distance[now] + cost :
-                distance[next_node] = distance[now] + cost
-                # n번째 라운드드 값에서도 갱신된다? 그러면 음수 사이클 존재 하는 거유 
-                if x == n -1:
-                    return True 
-    return False 
+    global n,m
+    # 노드
+    for i in range(1, n+1) :
+        for j in range(m) :
+            currNode = tList[j][0]
+            nextNode = tList[j][1]
+            cost = tList[j][2]
+            if distance[currNode] != INF and distance[nextNode] >distance[currNode]+ cost :
+                distance[nextNode] = distance[currNode]+ cost
+                if i == n :
+                    return True
+    return False
 
 
 
-dfResult = df(1)
-if dfResult : 
-    print(-1)
-else : 
-    for i in range(2,n+1) :
-        if distance[i] == (float('INF')) :
-            print(-1)
-        else :
-            print(distance[i])
+
+if __name__ == "__main__":
+    n , m = map(int , sys.stdin.readline().split())
+    distance = [INF] *(n+1)
+    tList = []
+
+    for _ in range(m):
+        a , b, c = map(int , sys.stdin.readline().split())
+        tList.append((a,b,c))
+
+    result = bef(1)
+    if result :
+        print(-1)
+    else :
+        for i in range(2,n+1) :
+            if distance[i] == INF :
+                print(-1)
+            else :
+                print(distance[i])
+
+
 
